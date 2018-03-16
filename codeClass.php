7b30a0c071a04404346a4424422ed3a5e1f3e797
AndyLQ <32448344+AndyLQ@users.noreply.github.com>
2018-03-16 13:43:47 +0100

    Creating codeClass.php

---
+++ codeClass.php
@@ -0,0 +1,42 @@
+<?php
+
+//connection credentials
+$servername = "sql.endora.cz:3313";
+$username = "baaacase2";
+$password = "case2BAAA";
+$dbname = "baaacase2";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$className = $_GET['className'];
+$email = $_GET['email'];
+
+$hash = (rand(100000,999999));
+
+$sql = "INSERT INTO class (ID, hash, class_name, email)
+VALUES (0, '$hash', '$className', '$email')";
+
+
+if ($conn->query($sql) === TRUE) {
+  header("Location: teachercode.php?hash=$hash");
+die();
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
+
+
+?>
+
+
+
