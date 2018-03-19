c04e453a7f602e36d528ab6179d231d4d71ee892
Petr <petr@petrmrkvicka.com>
2018-03-19 10:48:14 +0100

    Creating PHP code for joining class

---
+++ studentCodeProceed.php
@@ -0,0 +1,33 @@
+<?php
+
+//connection credentials
+$servername = "sql.endora.cz:3313";
+$username = "baaacase2";
+$password = "case2BAAA";
+$dbname = "baaacase2";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$hash = $_GET['Code'];
+
+$sql1 = "SELECT * FROM class WHERE hash = '$hash'";
+$result1 = mysql_query($sql1);
+
+if (mysql_num_rows($result1)==1) {
+header("Location: studentteam.html");
+setcookie("hash", "$hash");
+die;
+}
+else{
+  header("Location: studentcode.html");
+  alert("This class doesn't exist");
+}
+
+?>
